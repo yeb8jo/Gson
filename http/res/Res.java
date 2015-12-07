4b49e72fe7d6399c019eb5fd81b94149d4080a35
yeb8jo <uwububu@gmail.com>
2015-12-07 17:11:19 +0900

    Create Res.java

---
+++ http/res/Res.java
@@ -0,0 +1,29 @@
+public class Res {
+    public String data;
+    public int code;
+    public Exception e;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public Exception getE() {
+        return e;
+    }
+
+    public void setE(Exception e) {
+        this.e = e;
+    }
+}
